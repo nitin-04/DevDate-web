fba019a7e13bbd63ceb2b26f19c67a1edb7579d5
nitin bahuguna <nitinbahuguna1251@gmail.com>
2025-02-16 21:47:44 +0530

    feat: implement feed fetching with Redux and create feed slice

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,34 @@
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux"
+import { BASE_URL } from "../utils/constants";
+import { addfeed } from "../utils/feedSlice";
+import { useEffect } from "react";
 
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getfeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addfeed(res?.data?.data));
+    }
+    catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getfeed();
+  }, []);
+
   return (
     <div>
-      
+
     </div>
   )
 }
---
+++ src/utils/feedSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const feedSlice = createSlice({
+    name: "feed",
+    initialState: null,
+    reducers: {
+        addfeed: (state, action) => {
+            return action.payload;
+        },
+        removeFeed: (state, action) => null,
+    },
+
+});
+
+export const { addfeed } = feedSlice.actions;
+export default feedSlice.reducer;
